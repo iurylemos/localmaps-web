72eb381aa93b8af1ef22bc3a012a0f34ef61110b
Claudio Iury <iurylemos4@gmail.com>
2021-06-26 13:26:32 -0300

    put images to load categories in newpage and insert map

---
+++ src/utils/categories.ts
@@ -0,0 +1,22 @@
+export type Category = {
+  key: string;
+  label: string;
+  url: string;
+};
+
+export const categories: Category[] = [
+  { key: "basket", label: "Vestuário", url: "media/basket.png" },
+  { key: "gas", label: "Combustível", url: "media/gas.png" },
+  {
+    key: "hospital",
+    label: "Farmácia/Hospital",
+    url: "media/hospital.png",
+  },
+  { key: "pet", label: "Pet Shop", url: "media/pet.png" },
+  {
+    key: "restaurant",
+    label: "Restaurante",
+    url: "media/restaurant.png",
+  },
+  { key: "shopping", label: "Mercado", url: "media/shopping.png" },
+];
--- src/views/New/index.tsx
+++ src/views/New/index.tsx
@@ -1,4 +1,6 @@
+import { LatLngExpression } from "leaflet";
 import React from "react";
+import { useState } from "react";
 import Input from "../../components/New/Input";
 import {
   Button,
@@ -12,8 +14,24 @@ import {
   MapContainer,
   Section,
 } from "./styles";
+import { Marker, TileLayer } from "react-leaflet";
+import { categories, Category } from "../../utils/categories";
+
+type FormState = {
+  name: string;
+  description: string;
+  contact: string;
+  category: string;
+};
 
 const NewPage: React.FC = () => {
+  const [form, setForm] = useState<FormState>({
+    name: "",
+    description: "",
+    contact: "",
+    category: "",
+  });
+
   return (
     <Container>
       <Form>
@@ -22,9 +40,59 @@ const NewPage: React.FC = () => {
         <Input
           label="Nome do local"
           name="name"
-          value="algumacoisa"
-          onChange={() => {}}
+          value={form.name}
+          onChange={setForm}
         />
+        <Input
+          label="Descrição"
+          name="description"
+          value={form.description}
+          onChange={setForm}
+        />
+        <Input
+          label="Contato"
+          name="contact"
+          value={form.contact}
+          onChange={setForm}
+        />
+        <Section>Endereço</Section>
+        <MapContainer
+          center={
+            {
+              lat: 12,
+              lng: 23,
+            } as LatLngExpression
+          }
+          zoom={13}
+          whenCreated={() => {}}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[12, 23] as LatLngExpression} />
+        </MapContainer>
+        <Section>Categoria</Section>
+        <CategoryContainer>
+          {categories.map((item: Category, idx: number) => (
+            <CategoryBox
+              key={idx + Math.random()}
+              onClick={() => {
+                setForm((prev) => ({
+                  ...prev,
+                  category: item.key,
+                }));
+              }}
+              isActive={form.category === item.key ? true : false}
+            >
+              <CategoryImage src={item.url} />
+              {item.label}
+            </CategoryBox>
+          ))}
+        </CategoryContainer>
+        <ButtonContainer>
+          <Button type="submit">Salvar</Button>
+        </ButtonContainer>
       </Form>
     </Container>
   );
